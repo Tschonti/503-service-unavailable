8b2666cea2b89cbb4642477c1a5401c672e4cf5c
Sámuel Fekete <feketesamu@gmail.com>
2022-02-27 23:14:56 +0100

    projekt létrehozása és useCase diagram bemásolása

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    static public void main(String[] args) {
+        System.out.println("Csá világ");
+    }
+}
---
+++ test/MainTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void mainTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
