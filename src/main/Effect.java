6d869d06308512adbcf690a62c73b9eb148543aa
Jónás Jurásek <jurasekjonas12@gmail.com>
2022-03-27 18:09:48 +0200

    Effect javadoc

--- src/main/Effect.java
+++ src/main/Effect.java
@@ -1,10 +1,39 @@
 package main;
 import agents.Agent;
 
+/**
+ * This interface is implemented by everything that has any kind of impact to the Virologist, who has it.
+ */
 public interface Effect {
+    /**
+     * The Equipments impact at the start of the Virologists turn.
+     * @param to The Virologist on turn.
+     */
     void onTurnImpact(Virologist to);
+
+    /**
+     * The Equipments impact at the end of the Virologists turn.
+     * @param to The Virologist on turn.
+     */
     void endTurnImpact(Virologist to);
+    /**
+     * The Effect's impact in case of a Virologist throws an Agent on another Virologist.
+     * @param agent The used Agent.
+     * @param from The throwing Virologist.
+     * @param to The Virologist, who gets thrown.
+     */
     void counterImpact(Agent agent, Virologist from, Virologist to);
+
+    /**
+     * Tells if with this Equipment a Virologist is allowed to be robbed.
+     * Always returns false
+     * @return true, if a Virologist with this Equipment can be stolen from.
+     */
     boolean allowStealing();
+
+    /**
+     * Decrements the Effect's time. If it expires, removes itself from the Virologist.
+     * @param v The owner Virologist.
+     */
     void decrement(Virologist v);
 }
